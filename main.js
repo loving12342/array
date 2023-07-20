16bfff4e2b074de801adc9acd6710d9a890bd9d8
loving12342 <128096728+loving12342@users.noreply.github.com>
2023-07-19 22:11:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var string = "texto";
+console.log(string);
+
+var numero = 8888;
+console.log (numero);
+
+var array =["chocolate", "baunilha" ,"morango" ];
+console.log(array);
+console.log(array[1]);
+
+var nome = "pudino "
+var sobrenome = " de claras"
+var nomecompleto = nome.concat (sobrenome);
+console.log(nomecompleto)
+
+var comprimento = nomecompleto.length;
+console.log(comprimento);
+
+var novonome = nomecompleto.replace("de claras","de leite")
+console.log(novonome)
+
+var b =15.42703333321312;
+var c =262.144
+console.log("b")
+console.log("c")
+
+var soma = b+c;
+console.log(soma);
+var sub = b-c;
+console.log (sub)
+var mult = b*c;
+console.log(mult);
+var div = b/c;
+console.log (div);
